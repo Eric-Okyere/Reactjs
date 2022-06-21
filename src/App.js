9be92ac57c450cef320ff2e57bcc658a353b494a
Eric Okyere <102455856+Eric-Okyere@users.noreply.github.com>
2022-06-21 15:46:08 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+<div class="row">
+
+    <div class="col-md-12 Cont">
+        <div class="col-md-12 contf">0</div>
+
+    </div>
+
+<div class="col-md-12 Cont">
+    <div class="col-md-3 cont"><strong>AC</strong></div>
+    <div class="col-md-3 cont1"><strong>+/-</strong></div>
+    <div class="col-md-3 cont1"><strong>%</strong></div>
+    <div class="col-md-3 contb"><strong>÷</strong></div>
+</div>
+
+<div class="col-md-12 Cont">
+    <div class="col-md-3 cont"><strong>7</strong></div>
+    <div class="col-md-3 cont1"><strong>8</strong></div>
+    <div class="col-md-3 cont1"><strong>9</strong></div>
+    <div class="col-md-3 contb"><strong>x</strong></div>
+</div>
+
+<div class="col-md-12 Cont">
+    <div className="col-md-3 cont"><strong>4</strong></div>
+    <div className="col-md-3 cont1"><strong>5</strong></div>
+    <div className="col-md-3 cont1"><strong>6</strong></div>
+    <div className="col-md-3 contb"><strong>-</strong></div>
+</div>
+
+<div className="col-md-12 Cont">
+    <div className="col-md-3 cont"><strong>1</strong></div>
+    <div className="col-md-3 cont1"><strong>2</strong></div>
+    <div className="col-md-3 cont1"><strong>3</strong></div>
+    <div className="col-md-3 contb"><strong>+</strong></div>
+</div>
+
+<div className="col-md-12 Cont">
+    <div className="col-md-6 net"><strong>0</strong></div>
+    <div className="col-md-3 cont1"><strong>.</strong></div>
+    <div className="col-md-3 contb"><strong>=</strong></div>
+</div>
+</div>
+</div>
+  );
+}
+
+export default App;
